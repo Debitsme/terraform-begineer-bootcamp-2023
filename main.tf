aabcd5747c63827d0ae9e4b3bf606bb6008e1159
Hamza Ali <hamzaali.h7777@gmail.com>
2023-09-24 06:59:28 +0000

    #7 Create an S3 bucket along with a randomly generated string.

--- main.tf
+++ main.tf
@@ -1,1 +1,46 @@
+#To generate a random name
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
 
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+provider "aws" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  length  = 16
+  special = false
+  upper   = false
+  numeric = false
+
+}
+
+output "My_bucket_name" {
+  value = random_string.bucket_name.result
+}
+
+
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}

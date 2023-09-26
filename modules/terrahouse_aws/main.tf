cd6288a6080656ad5bc8611c2b42e86bb33b1475
Hamza Ali <hamzaali.h7777@gmail.com>
2023-09-26 17:45:49 +0000

    #14 Static_Website hosting

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "5.16.2"
     }
   }
-  
 }
 
 provider "aws" {
@@ -15,8 +14,42 @@ provider "aws" {
 # S3 storage
 resource "aws_s3_bucket" "bucket_1" {
   bucket = var.bucket_name
-
   tags = {
     userID = var.UUID
   }
+}
+
+# block for static website configurations
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.bucket_1.bucket
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+}
+
+#block to upload the index.html file
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.bucket_1.bucket
+  key    = "index.html"
+  #The issue here is that tfstate check the resource and not the data so if we 
+  #will change the data inside the index.htl and rerun it. Nothings will be changed.
+  # For this purpose we will use etags
+  source = "${path.root}/public/index.html"
+  #its a function that will create a hash based on the content of the file
+  etag = filemd5("${path.root}/public/index.html")
+}
+
+#block to upload the error.html file
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.bucket_1.bucket
+  key    = "error.html"
+  #The issue here is that tfstate check the resource and not the data so if we 
+  #will change the data inside the index.htl and rerun it. Nothings will be changed.
+  # For this purpose we will use etags
+  source = "${path.root}/public/error.html"
+  #its a function that will create a hash based on the content of the file
+  etag = filemd5("${path.root}/public/error.html")
 }
--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "My_bucket_name" {
   description = "for getting outputs from inside the modules"
   value = module.terrahouse.My_bucket_name
 }
+
+output "S3_website-endpoint" {
+  description = "s3_endpoint"
+  value = module.terrahouse.website-endpoint
+}
